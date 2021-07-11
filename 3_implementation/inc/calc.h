729d98ef89a41ac448efd3af35b7d16e77b60acb
vanshika ujjain <86082546+vanshika1103@users.noreply.github.com>
2021-07-12 04:30:36 +0530

    Update calc.h

--- 3_implementation/inc/calc.h
+++ 3_implementation/inc/calc.h
@@ -1,1 +1,30 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CALC_H__
+#define __CALC_H__
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] a 
+* @param[in] b 
+* @return Result of operand1 + operand2
+*/
+int add(int a,int b);
+int sub(int a,int b);
+int mul(int a,int b);
+int divi(int a,int b);
+int mod(int a,int b);
+int powe(int a,int b);
+int fact(int a);
+int permutation(int a,int b);
+int combination(int a,int b);
+void operations();
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
